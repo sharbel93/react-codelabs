0dbf5ea35731cc83bc7e6c438eb847ee32a48900
sharbel93 <sharbel@techfactory.ltd>
2019-12-18 13:35:45 +0300

    add card component and add styles to it

--- monsters-showcase/src/App.js
+++ monsters-showcase/src/App.js
@@ -20,12 +20,8 @@ class App extends React.Component {
   render() {
     return (
         <div className="App">
-          {/*<CardList name="sharbel chris"/> */}
-          <CardList name="sharbel chris">
-            { this.state.monsters.map(monster => (
-                <h1 key={ monster.id }> {monster.name} </h1>
-            ))}
-            </CardList>
+          {/* props === monsters */}
+          <CardList monsters={this.state.monsters} />
         </div>
     )
   }
