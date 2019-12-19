65db6fd4113c16231532a1f57056eab8d2734806
sharbel93 <sharbel@techfactory.ltd>
2019-12-19 15:48:10 +0300

    Updates

---
+++ precache-manifest.c84d498bc75eb3a44c76b97d8763ea76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08b76bfe3bd8817e2300054271ab79f",
+    "url": "/react-codelabs/index.html"
+  },
+  {
+    "revision": "3aada4283945cb60d2d7",
+    "url": "/react-codelabs/static/css/main.54d84da1.chunk.css"
+  },
+  {
+    "revision": "c13528568cb0e8751211",
+    "url": "/react-codelabs/static/js/2.13479aff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-codelabs/static/js/2.13479aff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3aada4283945cb60d2d7",
+    "url": "/react-codelabs/static/js/main.7f9b8779.chunk.js"
+  },
+  {
+    "revision": "9f688b75a0d7bf0b9039",
+    "url": "/react-codelabs/static/js/runtime-main.7eab4319.js"
+  }
+]);
